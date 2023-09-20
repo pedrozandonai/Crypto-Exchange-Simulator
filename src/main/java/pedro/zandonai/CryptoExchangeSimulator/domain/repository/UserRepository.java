df3405fb7898be3166d653cae91750cd9398b66a
pedrozandonai <pedrozandonai10@gmail.com>
2023-09-19 22:51:04 -0300

    Added UserRepository.java

---
+++ src/main/java/pedro/zandonai/CryptoExchangeSimulator/domain/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package pedro.zandonai.CryptoExchangeSimulator.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pedro.zandonai.CryptoExchangeSimulator.domain.models.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
