75a0d662d13c72c45c764b28e7ebb695ac47ef22
pedrozandonai <pedrozandonai10@gmail.com>
2023-09-21 19:50:41 -0300

    Excluded BuyAsset.java

--- src/main/java/pedro/zandonai/CryptoExchangeSimulator/domain/models/BuyAsset.java
+++
@@ -1,7 +0,0 @@
-package pedro.zandonai.CryptoExchangeSimulator.domain.models;
-
-import jakarta.persistence.Entity;
-
-@Entity(name = "tb_buyasset")
-public class BuyAsset extends AssetOperations{
-}
