acdcc2e37dfd4062b1a74462e464e8f06b2bf868
pedrozandonai <pedrozandonai10@gmail.com>
2023-09-21 19:49:26 -0300

    Deleted Asset Operations

--- src/main/java/pedro/zandonai/CryptoExchangeSimulator/domain/models/SellAsset.java
+++
@@ -1,7 +0,0 @@
-package pedro.zandonai.CryptoExchangeSimulator.domain.models;
-
-import jakarta.persistence.Entity;
-
-@Entity(name = "tb_sellasset")
-public class SellAsset extends AssetOperations{
-}
