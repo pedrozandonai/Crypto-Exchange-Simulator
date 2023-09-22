1aa2cc063fa08dd548cf7ca25c67c435723371d3
pedrozandonai <pedrozandonai10@gmail.com>
2023-09-22 16:15:51 -0300

    Final changes for the implementation on Railway

--- src/main/java/pedro/zandonai/CryptoExchangeSimulator/CryptoExchangeSimulatorApplication.java
+++ src/main/java/pedro/zandonai/CryptoExchangeSimulator/CryptoExchangeSimulatorApplication.java
@@ -1,11 +1,13 @@
 package pedro.zandonai.CryptoExchangeSimulator;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL!")})
 @SpringBootApplication
 public class CryptoExchangeSimulatorApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CryptoExchangeSimulatorApplication.class, args);
 	}
